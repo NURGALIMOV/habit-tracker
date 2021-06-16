fa438664517ae50163083e6b1400e1bbb2efae7b
Илшат Нургалимов <nurgalimov@technokratos.com>
2021-06-16 19:37:12 +0300

    add module notification

---
+++ backend/notification/src/test/java/ru/inurgalimov/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inurgalimov.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
