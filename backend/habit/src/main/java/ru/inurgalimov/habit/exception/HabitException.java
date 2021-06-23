9339f13292a075a9beb70a8ce7c48439b46e172b
Илшат Нургалимов <nurgalimov@technokratos.com>
2021-06-23 19:01:39 +0300

    prepare deploy

--- backend/habit/src/main/java/ru/inurgalimov/habit/exception/HabitException.java
+++ backend/habit/src/main/java/ru/inurgalimov/habit/exception/HabitException.java
@@ -4,5 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class HabitException extends RuntimeException {
+public class HabitException extends BaseAppException {
+
+    @Override
+    public HttpStatus getStatus() {
+        return HttpStatus.BAD_REQUEST;
+    }
 }
--- backend/habit/src/main/java/ru/inurgalimov/habit/exception/NotFoundException.java
+++ backend/habit/src/main/java/ru/inurgalimov/habit/exception/NotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends RuntimeException {
+public class NotFoundException extends BaseAppException {
 
     public NotFoundException() {
         super();
@@ -12,6 +12,11 @@ public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
         super(message);
+    }
+
+    @Override
+    public HttpStatus getStatus() {
+        return HttpStatus.NOT_FOUND;
     }
 
 }
